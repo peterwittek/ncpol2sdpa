b55b032a994a55fb9fbca71414712b952fc0064f
Peter Wittek <peterwittek@acm.org>
2013-04-11 16:27:36 +0800

    Test Hamiltonian added

--- ncmoments.m
+++ ncmoments.m
@@ -1,17 +1,17 @@
-function [Fmoments]=ncmoments(X, inequalities, equalities, ord)
+function [Fmoments, M]=ncmoments(X, inequalities, equalities, order)
 %NCMOMENTS - Converts noncommutative variables and sets of inequalities 
 % and equalities to moments and localizing matrices
 %
 %Based on the noncommutative example in Bermuja:
 %http://math.berkeley.edu/~philipp/Software/Moments
 %
-% Syntax:  [Fmoments] = ncmoments(X, inequalities, equalities, ord)
+% Syntax:  [Fmoments] = ncmoments(X, inequalities, equalities, order)
 %
 % Inputs:
 %    X            - Noncommutative variables (ncvar type from Yalmip)
 %    inequalities - Inequalities described in terms of X
 %    equalities   - Equalities described in terms of X
-%    ord          - Order of SDP relaxation
+%    order        - Order of SDP relaxation
 %
 % Outputs:
 %    Fmoments - Moments and localizing matrices
@@ -21,24 +21,27 @@ function [Fmoments]=ncmoments(X, inequalities, equalities, ord)
 %
 
 % Author: Philipp Rostalski, Peter Wittek
-% December 2012; Last revision: 13-Dec-2012
+% December 2012; Last revision: 04-Apr-2013
 
 %------------- BEGIN CODE --------------
 
 % Noncommutative monomial vectors needed to set up moment matrices
-mon = monolist(X,ord);
-
+mon = monolist(X,order);
+    
 % Define the highest order moment matrix (symmetric part)
 Maux = 1/2*(mon*mon'+(mon*mon').'); % I
 
 % Construct all submatrices of Maux, s.t. 'M_s(y)' is stored in M{s+1}
-for i=0:ord
+for i=0:order
     n_i(i+1) = 2^(i+1)-1;
     M{i+1} = Maux(1:n_i(i+1),1:n_i(i+1)); % compute moment matrices of lower order
 end
 % Add semidefinite constraints for the moment matrix
 % (a nonsymetric moment matrix is PSD iff its symmetric part is).
-F = set(M{ord+1} > 0); 
+F = set(M{order+1} > 0); 
+
+% ...but we also want M to be a symmetric matrix.
+F = F+ set(mon*mon'==(mon*mon').');
 
 for j=1:length(inequalities)
     % Define the localization matrix for the inequality constraint
@@ -52,9 +55,6 @@ end
 %% Computing a "Sylvester"-type matrix consisting of all polynomials and its shifts 
 % of degree deg(pol*x^alpha) <= 2t
 
-vec=[]; % initialize set of all linear equations and its shifted
-        % form (multiplied by some monomials) in a vector
-
 Feqconstr = set([]); % initialize constraint set (for SOLVESDP.m)
 
 % Create all element-wise constraints, and put its shift in a vector 
@@ -62,9 +62,8 @@ for i = 1:length(equalities)
      % add all possible equality constraints of degree smaller or equal
      % to t
      % XXXX Localizer = pol(i)*monolist(X,2*t-degree(pol(i)));     
-     Localizer = monolist(X,ord-ceil(1/2*degree(equalities(i))))*equalities(i)*monolist(X,ord-ceil(1/2*degree(equalities(i))))';
+     Localizer = monolist(X,order-ceil(1/2*degree(equalities(i))))*equalities(i)*monolist(X,order-ceil(1/2*degree(equalities(i))))';
      
-     vec = [vec;Localizer];
      Feqconstr = Feqconstr + set(Localizer==0); % and add as constraints
 end
 
