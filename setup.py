759ebe605d9749d4296af922db56b0546b3b2664
Peter Wittek <peterwittek@acm.org>
2014-04-29 12:00:03 +0200

    Setup script updated

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from distutils.core import setup
 
 setup(
-    name='ncpol2sdpa',
-    version='1.0',
-    author='P. Wittek',
-    packages=['ncpol2sdpa'],
-    url='http://peterwittek.github.io/ncpol2sdpa/',
-    license='LICENSE',
-    description='A converter from noncommutative polynomial optimization problems to sparse SDPA input format.',
-    long_description=open('README.rst').read(),
-    install_requires=[
+    name = 'ncpol2sdpa',
+    version = '1.0',
+    author = 'Peter Wittek',
+    author_email = 'dummy@address',
+    packages = ['ncpol2sdpa'],
+    url = 'http://peterwittek.github.io/ncpol2sdpa/',
+    keywords = ['sdp', 'semidefinite programming', 'relaxation', 'polynomial optimization problem', 'noncommuting variable', 'sdpa'],
+    license = 'LICENSE',
+    description = 'A converter from polynomial optimization problems of noncommutative variables to sparse SDPA input format.',
+    long_description = open('README.rst').read(),
+    classifiers = [
+         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+         'Operating System :: OS Independent',
+         'Development Status :: 4 - Beta',
+         'Topic :: Scientific/Engineering :: Mathematics',
+         'Topic :: Scientific/Engineering :: Physics',
+         'Intended Audience :: Science/Research',
+         'Programming Language :: Python'
+      ],
+    install_requires = [
         "sympy >= 0.7.2"
-    ],
+      ],
 ) 
