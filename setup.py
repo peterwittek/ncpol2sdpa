98b7e2bef04d3c91f40b15708a6154398dd61e21
Peter Wittek <peterwittek@acm.org>
2014-04-29 11:45:24 +0200

    README filename changed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='ncpol2sdpa',
-    version='1.0.0',
+    version='1.0',
     author='P. Wittek',
     packages=['ncpol2sdpa'],
     url='http://peterwittek.github.io/ncpol2sdpa/',
     license='LICENSE',
     description='A converter from noncommutative polynomial optimization problems to sparse SDPA input format.',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "sympy >= 0.7.2"
     ],
