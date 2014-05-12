9ae3d9f3c54733fdff0231bd51739b58ed2557fc
Peter Wittek <peterwittek@acm.org>
2014-05-12 09:47:24 +0200

    Manifest.in updated to include CHANGES

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ncpol2sdpa',
-    version='1.0',
+    version='1.1',
     author='Peter Wittek',
     author_email='dummy@address',
     packages=['ncpol2sdpa'],
